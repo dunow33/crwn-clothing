bd6cd606d61ab646b85c2153e235eebcce98e588
Brian Donnoe <dunow33@hotmail.com>
2020-02-20 06:06:58 -0500

    update menu items

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React from 'react';
+import { Switch, Route, BrowserRouter } from 'react-router-dom';
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component.jsx';
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+)
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <BrowserRouter>
+        <Switch>
+          <Route exact path='/' component={ HomePage } />
+          <Route exact path='/shop' component={ ShopPage } />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
