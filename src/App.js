347c1ce13740a33548603a80659db300af8fa206
Brian Donnoe <dunow33@hotmail.com>
2020-02-21 00:46:46 -0500

    added collection item component and collection preview component

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Switch, Route, BrowserRouter } from 'react-router-dom';
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
-import ShopPage from './pages/shop/shop.component.jsx';
+import ShopPage from './pages/shop/shop.component';
 
 const HatsPage = () => (
   <div>
