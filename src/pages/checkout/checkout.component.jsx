0416e5e90be503d424920995a797ac46a116f304
Brian Donnoe <dunow33@hotmail.com>
2020-03-18 18:35:05 -0400

    more updates

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -2,7 +2,10 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
-import { selectCartItems, selectCartTotal } from '../../redux/cart/cart.selectors';
+import CheckoutItem from '../../components/checkout-item/checkout-item.component';
+
+import { selectCartItems,
+		 selectCartTotal } from '../../redux/cart/cart.selectors';
 
 import './checkout.styles.scss';
 
@@ -25,11 +28,9 @@ const CheckoutPage = ({cartItems, total}) => (
 				<span>Remove</span>
 			</div>
 		</div>
-		{
-			cartItems.map(cartItem => 
-				cartItem.name
-			)
-		}
+		{cartItems.map(cartItem => (
+			<CheckoutItem key={ cartItem.id } cartItem={ cartItem } />
+		))}
 
 		<div className='total'>
 			<span>TOTAL: ${total}</span>
