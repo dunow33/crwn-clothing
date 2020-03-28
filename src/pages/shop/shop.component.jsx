1dbf13c3a77ef130f553e52d4365dbb94b46ce15
Brian Donnoe <dunow33@hotmail.com>
2020-03-27 20:23:03 -0400

    added redux persist

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,18 +1,8 @@
 import React from 'react';
 
-import SHOP_DATA from './shop.data.js';
-
 import CollectionPreview from '../../components/collection-preview/collection-preview.component';
 
-class ShopPage extends React.Component {
-	constructor(props) {
-		super(props);
-
-		this.state = {
-			collections: SHOP_DATA
-		};
-	}
-
+const ShopPage = ({ collections }) => {
 	render() {
 		const { collections } = this.state;
 
