68a7ec271840bca20d782c4e05cf36b35114045c
Brian Donnoe <dunow33@hotmail.com>
2020-04-03 02:32:37 -0400

    data normalization

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -4,5 +4,11 @@ const selectShop = state => state.shop;
 
 export const selectCollections = createSelector(
 	[selectShop],
-	shop => shop.collections
-)
+		shop => shop.collections
+);
+
+export const selectCollection = collectionUrlParam =>
+	createSelector(
+		[selectCollections],
+		collections => collections[collectionUrlParam]
+);
