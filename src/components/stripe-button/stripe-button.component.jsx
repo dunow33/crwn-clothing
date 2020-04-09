322fecea681c710c97293d294d7b34acbcd9f6ab
Brian Donnoe <dunow33@hotmail.com>
2020-04-09 01:24:15 -0400

    updating stripe integration

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+	const priceForStripe = price * 100;
+	const publishableKey = 'pk_test_l7EUOs6vLIDbDNg1Hd1atAAm00Xx6AwD0d';
+
+	const onToken = token => {
+		alert('Payment Successful');
+	}
+
+	return (
+		<StripeCheckout
+			label = 'Pay Now'
+			name = 'Crown Clothing Ltd'
+			billingAddress
+			shippingAddress
+			image='https://svgshare.com/i/CUz.svg'
+			description={ `Your total is $${price} `}
+			amount = { priceForStripe }
+			panelLabel = 'Pay Now'
+			token = { onToken }
+			stripeKey = { publishableKey }
+		/>
+	)
+}
+
+export default StripeCheckoutButton;
