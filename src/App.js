bcd0505e4bc24d600c5f714ce977dc7c5e5e0633
Brian Donnoe <dunow33@hotmail.com>
2020-04-09 21:12:19 -0400

    putting data into firebase

--- src/App.js
+++ src/App.js
@@ -11,9 +11,10 @@ import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up
 import CheckoutPage from './pages/checkout/checkout.component';
 
 import Header from './components/header/header.component';
-import { auth, createUserProfileDocument } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument, addCollectionAndDocuments } from './firebase/firebase.utils';
 import { setCurrentUser } from './redux/user/user.actions';
 import { selectCurrentUser } from './redux/user/user.selectors';
+import { selectCollectionsForPreview } from './redux/shop/shop.selectors';
 
 
 class App extends React.Component {
@@ -34,6 +35,7 @@ class App extends React.Component {
           });
       }
       setCurrentUser(userAuth);
+      addCollectionAndDocuments('collections', collectionsArray);
     });
   }
 
@@ -67,7 +69,8 @@ class App extends React.Component {
 }
 
 const mapStateToProps = createStructuredSelector({
-   currentUser: selectCurrentUser
+   currentUser: selectCurrentUser,
+   collectionsArray: selectCollectionsForPreview
 })
 
 const mapDispatchToProps = dispatch => ({
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -39,7 +39,9 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
 	return userRef;
 };
 
-firebase.initializeApp(config);
+export const addCollectionAndDocuments = (collectionKey, objectsToAdd) => {
+	const collectionRef = firestore.collection(collectionKey);
+}
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
